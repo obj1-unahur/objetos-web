6fba8e3d24e44c067a41b5055f6585ad4c50a364
ValentinoChiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2024-05-22 11:38:46 -0300

    Mi perfil

--- src/paths.js
+++ src/paths.js
@@ -3,3 +3,4 @@ export const PATH_CURSADA = `${PATH}/Cursada`
 export const PATH_CONTRATO_PEDAGOGICO = `${PATH}/contrato-pedagogico`
 export const PATH_VIDEOS = `${PATH}/Videos`
 export const PATH_MATERIAL = `${PATH}/material`
+export const PATH_PERFIL = `${PATH}/mi-perfil`
